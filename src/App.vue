9b163d2a09dabb0ce2a3e696f8d07a2ec1fffba4
Dipokal HHJ <hhj@devent.kr>
2022-09-06 22:48:51 +0900

    feat: apply tailwindcss
    + tailwindcss 적용

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+
 </template>
 
 <script>
@@ -10,14 +8,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'tailwindcss/tailwind.css';
+import './style.css'
 
 Vue.config.productionTip = false
 
