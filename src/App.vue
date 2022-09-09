5738cfc76e636d680876c9f327b37ed58f1d86b7
Dipokal HHJ <hhj@devent.kr>
2022-09-09 11:53:29 +0900

    feat: split components
    + 컴포넌트 분리

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+<div class="container mx-auto px-4 py-2">
+  <Header />
+
+  <div class="h-full">
+
+  </div>
+
+</div>
 
 </template>
 
 <script>
+import Header from './components/Header.vue'
+
+
 export default {
-  name: 'App'
+  name: 'App',
+    components: {
+      Header
+  }
 }
 </script>
